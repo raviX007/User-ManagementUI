3083f60ec0422846d781744b77123b57e169cdf6
raviX007 <91694681+raviX007@users.noreply.github.com>
2025-01-17 21:37:22 +0530

    Update config.ts

--- app/config.ts
+++ app/config.ts
@@ -1,5 +1,5 @@
 const config = {
-    apiBaseUrl: 'https://choreoproduserappsflamingo.azurecr.io/d35dbe24b706-user-management-backend-configs:960ea9b7c01a37ed482e0d654608d115568f86e7/api/users',
+    apiBaseUrl: 'https://user-management-backend-1.onrender.com/api/users',
   };
   
   export default config;
