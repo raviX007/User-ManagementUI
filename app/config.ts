fa9a66ba8d99eaba455fd127cddb514e695df897
raviX007 <91694681+raviX007@users.noreply.github.com>
2025-01-17 06:29:38 +0530

    Update config.ts

--- app/config.ts
+++ app/config.ts
@@ -1,5 +1,5 @@
 const config = {
-    apiBaseUrl: 'https://user-management-backend-production.up.railway.app/api/users',
+    apiBaseUrl: 'https://choreoproduserappsflamingo.azurecr.io/d35dbe24b706-user-management-backend-configs:960ea9b7c01a37ed482e0d654608d115568f86e7/api/users',
   };
   
   export default config;
